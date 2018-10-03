df9f8c6b9ab3f3ff82ef13068a7ef8b65da51dc8
Kilian Beuchard <beu.kilian@gmail.com>
2018-10-03 17:17:43 +0200

    Deleted __eq__ in both class and deleted tests in if main

--- geometry.py
+++ geometry.py
@@ -3,9 +3,6 @@ class Rectangle():
 		self.top = top
 		self.bot = bot
 
-	# def __eq__(self, other):
-	# 	return self.top == other.top and self.bot == other.bot
-
 	def __repr__(self):
 		return 'Rectangle(top: {}, bot: {})'.format(self.top, self.bot)
 
@@ -35,33 +32,7 @@ class Point():
 		self.x = x
 		self.y = y
 
-	# def __eq__(self, other):
-	# 	return self.x == other.x and self.y == other.y
-
 	def __repr__(self):
 		return 'Point(x : {}, y : {})'.format(self.x, self.y)
-
-if __name__ == '__main__':
-	top_p1 = Point(3, 7)
-	bot_p1 = Point(7, 3)
-	rect_1 = Rectangle(top_p1, bot_p1)
-
-	top_p2 = Point(1, 6)
-	bot_p2 = Point(6, 1)
-	rect_2 = Rectangle(top_p2, bot_p2)
-
-	top_p3 = Point(4, 5)
-	bot_p3 = Point(5, 6)
-	rect_3 = Rectangle(top_p3, bot_p3)
-
-	print(rect_2.area)
-	print(rect_1.get_common_area(rect_2))
-
-	# print(rect_1)
-	# print(rect_1.width, rect_1.height, rect_1.area)
-	# rect_1.bot = bot_p3
-
-	# print(rect_1)
-	# print(rect_1.width, rect_1.height, rect_1.area)
 
 
